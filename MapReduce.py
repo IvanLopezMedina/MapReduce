dc08c56186dd491016ac06ccb6ab3dbe408c3dec
Iván López Medina <ivan.lopez.medina.ilm@gmail.com>
2019-02-12 15:58:05 +0100

    MapReduce base

---
+++ MapReduce.py
@@ -0,0 +1,13 @@
+class MapReduce:
+
+    def __init__(self):
+        self.test = 0
+
+    def helloWorld(self):
+        print "Hello World"
+
+if __name__ == '__main__':
+    my_MapReduce = MapReduce()
+    my_MapReduce.helloWorld()
+
+
