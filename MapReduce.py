e87d539987faa305ed4f31b154f3cd7f7f594afe
Iván López Medina <ivan.lopez.medina.ilm@gmail.com>
2019-02-13 09:22:39 +0100

    Base Functions
    Split done, still needs to be cleaned from unwanted characters

--- MapReduce.py
+++ MapReduce.py
@@ -3,11 +3,37 @@ class MapReduce:
     def __init__(self):
         self.test = 0
 
-    def helloWorld(self):
-        print "Hello World"
+    def readFile(self):
+        f = open("file1.txt", "r")
+        lines = []
+        for x in f:
+            lines.append(x)
+        #If you want to read each character one by one use print(f.read(1)) excluding the unwanted characters
+        return lines
+
+    def split(self,lines):
+        splitted = []
+        for line in lines:
+            splitted.append(line.split())
+
+        return splitted
+
+    def map(self):
+	    print ""
+
+    def shufle(self):
+	    print ""
+
+    def reduce(self):
+	    print ""
 
 if __name__ == '__main__':
     my_MapReduce = MapReduce()
-    my_MapReduce.helloWorld()
+    lines = my_MapReduce.readFile()
+    splitlines = my_MapReduce.split(lines)
+
+    for line in splitlines:
+        for word in line:
+            print word
 
 
