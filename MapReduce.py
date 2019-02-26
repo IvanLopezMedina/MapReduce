acb1cf340f80cfe79d153bf30d55048ebc44e4c9
Iván López Medina <ivan.lopez.medina.ilm@gmail.com>
2019-02-26 09:41:11 +0100

    No threads. Optimización de lectura, add file param y mapping

--- MapReduce.py
+++ MapReduce.py
@@ -1,18 +1,16 @@
 import re
 import time
-import threading
+import sys
 class MapReduce:
 
     def __init__(self):
         self.test = 0
 
-    def readFile(fn):
+    def readFile(self):
         regexp = '[*/.,:;.0-9]'
         bufsize = 5000000
-        lock = threading.Lock()
         linesread = []
         splitted = []
-        i = 0
         with open("bigfile.txt", "rb") as infile:
             while True:
                 lines = re.sub(regexp, '', infile.read(bufsize))
@@ -25,14 +23,11 @@ class MapReduce:
                     else :
                         lines += char
                         #Launch thread
-                lock.acquire()
                 linesread.append(lines)
-                t = threading.Thread(target=my_MapReduce.split(linesread, splitted))
-                t.start()
-                #print "new thread"
+                #Launch threads
+                #t = threading.Thread(target=my_MapReduce.split(linesread, splitted)       
+                my_MapReduce.split(linesread, splitted)
                 linesread = []
-                lock.release()
-        #my_MapReduce.split(linesread, splitted)
         infile.close()
         return splitted
 
@@ -53,7 +48,7 @@ if __name__ == '__main__':
     start = time.time()
     my_MapReduce = MapReduce()
     lines = my_MapReduce.readFile()
-
+    
     #for line in lines:
     #    for word in line:
     #        print word
